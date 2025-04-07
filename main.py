7dbe3945284112fcf049cbe8dd78d7464053a923
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-04-07 17:12:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -357,4 +357,4 @@ async def get_pending_queries():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
